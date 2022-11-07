62746833f160a44c83b740ad4aeb41ee19e8697b
Tinker <1969094791@qq.com>
2022-11-08 03:58:14 +0800

    2022-11-08 03:58

--- Group14-admin-cinemaControl-service/src/main/java/com/example/group14admincinemacontrolservice/service/CinemaSevice.java
+++ Group14-admin-cinemaControl-service/src/main/java/com/example/group14admincinemacontrolservice/service/CinemaSevice.java
@@ -6,14 +6,27 @@ import com.example.group14admincinemacontrolservice.entity.Cinema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CinemaSevice {
     @Autowired
-    private CinemaDao dao;
-    public boolean InsertCinema(Cinema cinema){
-        if(dao.Insert(cinema)){
-            return true;
-        }
-        else return false;
+    private CinemaDao cinemaDao;
+    public void InsertCinema(Cinema cinema){
+        cinemaDao.insert(cinema);
+    }
+    public void UpdateCinema(Cinema cinema){
+        cinemaDao.update(cinema);
+    }
+    public void deleteCinema(int cinemaId){
+        cinemaDao.delete(cinemaId);
+    }
+    public List<Object> selectCinemaId(){
+        return cinemaDao.selectCinemaId();
+    }
+    public Cinema selectCinemaById(int cinemaId){
+        Cinema c=new Cinema();
+        c=cinemaDao.selectCinemaById(cinemaId);
+        return cinemaDao.selectCinemaById(cinemaId);
     }
 }
--- Group14-admin-cinemaControl-service/src/main/java/com/example/group14admincinemacontrolservice/service/SessionSevice.java
+++ Group14-admin-cinemaControl-service/src/main/java/com/example/group14admincinemacontrolservice/service/SessionSevice.java
@@ -1,40 +1,30 @@
-package com.example.group14admincinemacontrolservice;
+package com.example.group14admincinemacontrolservice.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.example.group14admincinemacontrolservice.dao.CinemaDaoImpl;
-import com.example.group14admincinemacontrolservice.dao.MovieDaoImpl;
 import com.example.group14admincinemacontrolservice.dao.SessionDao;
-import com.example.group14admincinemacontrolservice.dao.SessionDaoImpl;
 import com.example.group14admincinemacontrolservice.entity.Cinema;
 import com.example.group14admincinemacontrolservice.entity.Movie;
 import com.example.group14admincinemacontrolservice.entity.Session;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Type;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-@SpringBootTest
-class Group14AdminCinemaControlServiceApplicationTests {
-
+@Service
+public class SessionSevice {
     @Autowired
     private SessionDao sessionDao;
-    @Test
-    void contextLoads() {
-        Movie movie=new Movie();
-        movie=sessionDao.selectDate(1).get(0);
+    public boolean DateVaildate(String s,int movieId){
+        Movie movie=sessionDao.selectDate(movieId).get(0);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         Date date=null;
         Date releaseDate=null;
         Date endDate=null;
         try {
-            date = format.parse("2022-08-11");
+            date = format.parse(s);
             releaseDate=format.parse(movie.getReleaseDate());
             endDate=format.parse(movie.getEndDate());
             System.out.println(date);
@@ -43,8 +33,21 @@ class Group14AdminCinemaControlServiceApplicationTests {
         } catch (ParseException e) {
             System.out.println(e.getMessage());
         }
-        if(date.after(releaseDate)&&date.before(endDate)) System.out.println("1");
-        else System.out.println("0");
+        if(date.after(releaseDate)&&date.before(endDate)) return true;
+        else return false;
     }
-
+    public Map<String,Object>insertSession(Session session){
+        Map<String,Object>mp=new HashMap<>();
+        if (!DateVaildate(session.getScreeningDate(),session.getMovieId())){
+            mp.put("success",0);
+            mp.put("info","拍片日期不合适");
+            return mp;
+        }
+        else {
+            sessionDao.insertSession(session);
+            mp.put("success",1);
+            mp.put("info","排片成功");
+            return mp;
+        }
+    }
 }
