c2860836e200883b78e8a762478b31d8c94cfc5a
aliyun2857363464-xgF5O <916401886@qq.com>
2022-11-08 16:17:07 +0800

    redis共享session

---
+++ Group14-login-service/src/main/java/com/example/group14loginservice/Group14LoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.group14loginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group14LoginServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group14LoginServiceApplication.class, args);
+    }
+
+}
---
+++ Group14-login-service/src/test/java/com/example/group14loginservice/Group14LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group14loginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group14LoginServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
